4362bcccd076f23d09cdd98b701cbf5ca94da017
DQNEO <dqneoo@gmail.com>
2016-01-26 02:06:02 +0900

    return response

--- index.php
+++ index.php
@@ -1,11 +1,25 @@
 <?php
 
+class Response
+{
+    public $body;
+
+    public function __construct($msg)
+    {
+        $this->body = $msg;
+    }
+
+}
+
+/**
+ * @return Response
+ */
 function main()
 {
-    $msg = "hello world"
+    $msg = new Response("hello world");
     return $msg;
 }
 
 $msg = main();
-echo $msg;
+echo $msg->body;
 
